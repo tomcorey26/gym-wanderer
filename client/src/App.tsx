dee86e28225f2e6406187fb531d4a5f291dc7388
Thomas Corey <tcorey@MIT.local>
2020-01-30 15:48:34 -0500

    moved foldre lol

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Maps from "./components/Maps/Maps";
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1></h1>
+      <h1>mep</h1>
+      <Maps />
     </div>
   );
 };
---
+++ client/src/components/Maps/Maps.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Maps: React.FC = () => {
+  return (
+    <div>
+      <h2>Google maps shiz</h2>
+    </div>
+  );
+};
+
+export default Maps;
