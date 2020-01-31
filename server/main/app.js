6221ae30a9f5499793a72dd102e38a46e10e5d31
tomcorey26 <tomcorey26@gmail.com>
2020-01-30 23:23:53 -0500

    boiler plate server created

---
+++ server/main/app.js
@@ -0,0 +1,20 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", (req, res) => {
+  res.send("poop");
+});
+// app.use("/users", usersRouter);
+
+module.exports = app;
