4ca90d8b16c297cd875b053974c716b8bea6c6cf
Thomas Corey <tcorey@MIT.local>
2020-02-05 12:25:03 -0500

    google maps added google maps api

--- client/src/components/Maps/Maps.tsx
+++ client/src/components/Maps/Maps.tsx
@@ -35,37 +35,39 @@ const Maps: React.FC = () => {
 
   return (
     <LoadScript libraries={libraries} id="script-loader" googleMapsApiKey={Key}>
-      <GoogleMap
-        center={isUserInput ? center : location}
-        zoom={zoom}
-        mapContainerStyle={{
-          height: "400px",
-          width: "800px"
-        }}
-        id="example-map"
-      >
-        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-          <input
-            type="text"
-            placeholder="Customized your placeholder"
-            style={{
-              boxSizing: `border-box`,
-              border: `1px solid transparent`,
-              width: `240px`,
-              height: `32px`,
-              padding: `0 12px`,
-              borderRadius: `3px`,
-              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-              fontSize: `14px`,
-              outline: `none`,
-              textOverflow: `ellipses`,
-              position: "absolute",
-              left: "50%",
-              marginLeft: "-120px"
-            }}
-          />
-        </Autocomplete>
-      </GoogleMap>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <GoogleMap
+          center={isUserInput ? center : location}
+          zoom={zoom}
+          mapContainerStyle={{
+            height: "40%",
+            width: "40%"
+          }}
+          id="example-map"
+        >
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <input
+              type="text"
+              placeholder="Customized your placeholder"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                left: "50%",
+                marginLeft: "-120px"
+              }}
+            />
+          </Autocomplete>
+        </GoogleMap>
+      </div>
     </LoadScript>
   );
 };
