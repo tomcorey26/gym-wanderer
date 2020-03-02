d180a996e681fb38dfb6de4196053e4be7007383
Thomas Corey <tcorey@MIT.local>
2020-03-02 15:44:17 -0500

    Created gym pic Gallery component

---
+++ client/src/assets/photos.js
@@ -0,0 +1,33 @@
+export const photos = [
+    {
+      src: "https://images.unsplash.com/photo-1547919307-1ecb10702e6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      width: 4,
+      height: 6
+    },
+    {
+      src: "https://images.unsplash.com/photo-1558611848-73f7eb4001a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://images.unsplash.com/photo-1576678927484-cc907957088c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://images.unsplash.com/photo-1561214078-f3247647fc5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      width: 4,
+      height: 3
+    }
+  ];
+  
---
+++ client/src/components/GymPicGallery.tsx
@@ -0,0 +1,44 @@
+import React, {useState,useCallback} from 'react'
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+
+interface GymPicGalleryProps {
+    photos: any;
+}
+
+const GymPicGallery: React.FC<GymPicGalleryProps> = ({photos}) => {
+    const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div style={{width: "50vw"}}>      
+    <Gallery photos={photos} onClick={openLightbox} />
+    <ModalGateway>
+      {viewerIsOpen ? (
+        <Modal onClose={closeLightbox}>
+          <Carousel
+            currentIndex={currentImage}
+            views={photos.map(x => ({
+              ...x,
+              srcset: x.srcSet,
+              caption: x.title
+            }))}
+          />
+        </Modal>
+      ) : null}
+    </ModalGateway>
+    </div>
+  )
+}
+export default GymPicGallery
