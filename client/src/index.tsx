edae0e55e77cf5eaf86a01cf58804aa089c28d8d
tomcorey26 <tomcorey26@my.uri.edu>
2020-03-08 19:20:53 -0400

    got started with graph ql

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Server } from "miragejs";
 import { mockGymsApi } from "./mock";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
 import "./styles/globals.scss";
 
 new Server({
@@ -18,7 +22,22 @@ new Server({
   }
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "https://graphql-pokemon.now.sh/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- client/src/pages/CreateGym.tsx
+++ client/src/pages/CreateGym.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// kinda like the part 1 part 2 part 3 thing that the discover website has
+//swipe animation when going to the next form
+
 const CreateGym: React.FC = () => {
   return (
     <div>
