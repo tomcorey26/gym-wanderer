6c7f3ffc5f375623f786876603d685c65d668b36
tomcorey26 <tomcorey26@my.uri.edu>
2020-03-11 17:28:25 -0400

    access token working

---
+++ server/src/isAuth.ts
@@ -0,0 +1,27 @@
+import { MiddlewareFn } from 'type-graphql';
+import { MyContext } from './MyContext';
+import { verify } from 'jsonwebtoken';
+
+//bearer 4542930548-35
+
+export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+  const authorization = context.req.headers['authorization'];
+
+  if (!authorization) {
+    throw new Error('not authenticated');
+  }
+
+  try {
+    const token = authorization.split(' ')[1];
+    //the payload is whatever we used to sign our token
+    //(the object we passed to sign())
+    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+    context.payload = payload as any;
+  } catch (err) {
+    console.log(err);
+    throw new Error('not authenticated');
+  }
+
+  //next to move on to the next middleware
+  return next();
+};
--- server/src/userResolver.ts
+++ server/src/userResolver.ts
@@ -1,9 +1,87 @@
-import { Resolver, Query } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+  Ctx,
+  UseMiddleware
+} from 'type-graphql';
+import { User } from './entity/User';
+import { hash, compare } from 'bcryptjs';
+import { MyContext } from './MyContext';
+import { createRefreshToken, createAccessToken } from './auth';
+import { isAuth } from './isAuth';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return 'hello world';
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Could not find User');
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('bad password');
+    }
+
+    res.cookie('jid', createRefreshToken(user), {
+      httpOnly: true
+    });
+
+    return {
+      accessToken: createAccessToken(user)
+    };
   }
 }
