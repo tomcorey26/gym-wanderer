e844d94a939ee53864c60550e9a59a438c9a9704
tomcorey26 <tomcorey26@my.uri.edu>
2020-04-09 18:54:05 -0400

    Added Better ui to login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useLoginMutation, MeDocument, MeQuery } from '../generated/graphql';
 import { setAccessToken } from '../accessToken';
+import { Formik } from 'formik';
+import { FormContainer } from '../components/FormComponents/FormContainer';
+import { Button } from '@material-ui/core';
+import { isObjectEmpty } from '../utils';
+import { FormField } from '../components/FormComponents/FormField';
 
 //we get route props because this component is passed
 // as a prop to the react-router-dom <Route/> component
 export const Login: React.FC<RouteComponentProps> = ({ history }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [login] = useLoginMutation();
-  const handleSubmit = e => {
-    e.preventDefault();
-    return;
-  };
+  const showSubmit = true;
 
   return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault();
-        console.log('form submitted');
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      onSubmit={async ({ email, password }, { setSubmitting }) => {
+        setSubmitting(true);
         const response = await login({
           variables: {
             email,
-            password
+            password,
           },
           update: (store, { data }) => {
             if (!data) {
@@ -31,12 +34,13 @@ export const Login: React.FC<RouteComponentProps> = ({ history }) => {
             store.writeQuery<MeQuery>({
               query: MeDocument,
               data: {
-                me: data.login.user
-              }
+                me: data.login.user,
+              },
             });
-          }
+          },
         });
 
+        setSubmitting(false);
         console.log(response);
 
         if (response && response.data) {
@@ -46,21 +50,25 @@ export const Login: React.FC<RouteComponentProps> = ({ history }) => {
         history.push('/');
       }}
     >
-      <div>
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="password"
-          onChange={e => setPassword(e.target.value)}
-        />
-        <button type="submit"> login</button>
-      </div>
-    </form>
+      {({ values, isSubmitting, errors }) => (
+        <FormContainer>
+          <FormField placeholder="Email" name="email" type="input" />
+          <FormField placeholder="Password" name="password" type="password" />
+
+          {showSubmit && (
+            <Button
+              disabled={isSubmitting || !isObjectEmpty(errors)}
+              type="submit"
+              variant="contained"
+              color="secondary"
+            >
+              Login
+            </Button>
+          )}
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+          <pre>{JSON.stringify(errors, null, 2)}</pre>
+        </FormContainer>
+      )}
+    </Formik>
   );
 };
