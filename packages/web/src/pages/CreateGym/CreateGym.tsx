fe78b8dea9235cd26ca0196fa4bc1d0d03fc4bc0
Tom Corey <tomcorey26@my.uri.edu>
2020-04-15 16:22:45 -0400

    create gym ui

---
+++ packages/web/src/pages/CreateGym/CreateGym.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { Formik } from 'formik';
+import { FormContainer } from '../../components/FormComponents/FormContainer';
+import { FormField } from '../../components/FormComponents/FormField';
+import { FormPageControl } from '../../components/FormPageControl';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
+//we get route props because this component is passed
+// as a prop to the react-router-dom <Route/> component
+const mock = {
+  email: 'devito@gmail.com',
+  password: 'Test123@',
+};
+
+const pages: JSX.Element[] = [<Page1 />, <Page2 />];
+
+export const CreateGym: React.FC<RouteComponentProps> = ({ history }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  return (
+    <Formik
+      initialValues={{
+        gym_name: '',
+        description: '',
+        membership_cost: '',
+        phone: '',
+        equipment: [],
+        type: '',
+        city: '',
+        state: '',
+        zip_code: '',
+      }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ values, isSubmitting, errors }) => (
+        <FormContainer title="Create Gym">
+          {pages[currentPage]}
+          <FormPageControl
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            length={pages.length}
+            isSubmitting={isSubmitting}
+            errors={errors}
+          />
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+          <pre>{JSON.stringify(errors, null, 2)}</pre>
+        </FormContainer>
+      )}
+    </Formik>
+  );
+};
---
+++ packages/web/src/pages/CreateGym/Page2.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Page2Props {}
+
+export const Page2: React.FC<Page2Props> = ({}) => {
+  return (
+    <div>
+      <h1>page2</h1>
+    </div>
+  );
+};
